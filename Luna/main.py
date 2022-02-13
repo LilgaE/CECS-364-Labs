109586cf5e356f461385d7dea6e20d7159e0d442
LilgaE <lilga.evan@gmail.com>
2022-02-13 16:21:37 -0600

    Compare test implemented

--- Luna/main.py
+++ Luna/main.py
@@ -1,7 +1,28 @@
 # Writen by Stabs-
 # Beginnings of the infamous Luna
-# Stock Trading Bot
+# The Stock Trading Bot
 
+
+# To do list
+# Get current price data for the day to find when to buy/sell that day
+# How Fast can we get the data from?
+# Create a comparison function
+# List of top 10 stocks of the day
+# List of top 10 stocks of the Week
+# List of top 10 stocks of the Month
+# List of top 10 stocks of the Year
+# For above use present change
+# Challenges going through all 39952 stocks, 82221 funds, 11403 ETFs
+# Tax calculations for selling short term vs long term stock.
+# Dividend return on stock
+# Dividend tax information
+# Candle stick graph
+#
+#
+# Maybe Crypto a HFT
+# Maube some tax stuff
+#
+#
 
 from _socket import gethostname  # For Users Name
 import investpy  # Gets stock data
@@ -11,6 +32,9 @@ import datetime  # Gets current date
 
 
 # test0 are the example functions given from https://investpy.readthedocs.io/_info/usage.html
+from investpy import get_stocks_list
+
+
 def test0():
     Stockname = str(input("Please input the company name or ticker\n"))
     search_result = investpy.search_quotes(text=Stockname, products=['stocks'],
@@ -63,7 +87,7 @@ def test1():
     print("\n\n")
 
 
-# test2 is a combination of investpy and matplotlib.pyplot library to visualize the data better.
+# Plotting is a combination of investpy and matplotlib.pyplot library to visualize the data better.
 def plotting():
     Stockname = str(input("Please input the company name\n"))
     current = datetime.datetime.now()
@@ -82,6 +106,34 @@ def plotting():
     plt.show()
 
 
+def compare():
+    listofcount = []
+    listofnames = []
+    listofmeans = []
+    current = datetime.datetime.now()
+    cur = str(current.day) + '/' + str(current.month) + '/' + str(current.year)
+    pastyear = str(current.day) + '/' + str(current.month) + '/' + str(current.year - 1)
+    pastmonth = str(current.day) + '/' + str(current.month - 1) + '/' + str(current.year)
+    pastweek = str(current.day - 7) + '/' + str(current.month) + '/' + str(current.year)
+    with open("Symbollist .txt", "r") as Filein:
+        for row in Filein:
+            split = row.split(",")
+            listofnames.append(split[7])
+    for name in listofnames:
+        try:
+            search_result = investpy.search_quotes(text=name, products=['stocks'], n_results=1)
+            recent_data = search_result.retrieve_historical_data(from_date=pastweek, to_date=cur)
+            listofmeans.append(recent_data.mean())
+        except RuntimeError:
+            print("Stocks not found.")
+        except ConnectionError:
+            print(listofmeans)
+            print("connection error")
+            break
+
+
+        #print("\n\nAverage return for past year")
+        #print(str(recent_data['Change Pct'].mean()) + "%")
 # main asks what function the user would like to use and runs that function, also repeats until the user is done
 def main():
     pd.set_option('display.max_columns', None)
@@ -91,6 +143,7 @@ def main():
         print("1. Demo function")
         print("2. Messing around")
         print("3. PLOT")
+        print("4. compare")
         print("0. EXIT")
         Userin = int(input("Please input the test you would like to run\n"))
         if Userin == 1:
@@ -102,10 +155,13 @@ def main():
         elif Userin == 3:
             plotting()
             print("\n\n")
+        elif Userin == 4:
+            compare()
+            print("\n\n")
         else:
             print("Please Try again")
 
-    print("Thank you for using Luna the stock trading bot\n")
+    print("Thank you for using the stock trading bot\n")
     print("And never forget\n")
     print(
         "\"Nobody knows if a stock is going to go up, down,\nsideways or in fucking circles,\nleast of all "
